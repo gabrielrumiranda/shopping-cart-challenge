b405b46783fd5e0f6e0d070a963f12168841db2c
Gabriel Miranda <gabrielrumiranda@gmail.com>
2019-09-24 17:32:02 -0300

    Configure rubocop and set product seed

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Api::ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :update, :destroy]
+  before_action :set_product, only: %i[show update destroy]
 
   # GET /products
   # GET /products.json
@@ -9,8 +11,7 @@ class Api::ProductsController < ApplicationController
 
   # GET /products/1
   # GET /products/1.json
-  def show
-  end
+  def show; end
 
   # POST /products
   # POST /products.json
@@ -41,13 +42,14 @@ class Api::ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.fetch(:product, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.fetch(:product, {})
+  end
 end
--- test/controllers/api/products_controller_test.rb
+++ test/controllers/api/products_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Api::ProductsControllerTest < ActionDispatch::IntegrationTest
@@ -5,30 +7,30 @@ class Api::ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = products(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get api_products_url, as: :json
     assert_response :success
   end
 
-  test "should create product" do
+  test 'should create product' do
     assert_difference('Product.count') do
-      post api_products_url, params: { product: {  } }, as: :json
+      post api_products_url, params: { product: {} }, as: :json
     end
 
     assert_response 201
   end
 
-  test "should show product" do
+  test 'should show product' do
     get api_product_url(@product), as: :json
     assert_response :success
   end
 
-  test "should update product" do
-    patch api_product_url(@product), params: { product: {  } }, as: :json
+  test 'should update product' do
+    patch api_product_url(@product), params: { product: {} }, as: :json
     assert_response 200
   end
 
-  test "should destroy product" do
+  test 'should destroy product' do
     assert_difference('Product.count', -1) do
       delete api_product_url(@product), as: :json
     end
